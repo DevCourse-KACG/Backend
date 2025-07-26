595e4ac69b62b76804ee96f195f8c3e6217c1ae2
qkek123 <qkek622378@gmail.com>
2025-07-26 10:19:28 +0900

    feat: yml의 비밀키를 사용하도록 AuthService 변경

--- src/main/java/com/back/domain/auth/service/AuthService.java
+++ src/main/java/com/back/domain/auth/service/AuthService.java
@@ -1,7 +1,7 @@
 package com.back.domain.auth.service;
 
+import com.back.global.config.jwt.JwtProperties;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
@@ -10,18 +10,24 @@ import java.util.Date;
 
 @Service
 public class AuthService {
-    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final Key secretKey;
+    private final long expirationSeconds;
+
+    public AuthService(JwtProperties jwtProperties) {
+        this.secretKey = Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes());
+        this.expirationSeconds = Long.parseLong(jwtProperties.getExpirationSeconds());
+    }
 
     public String generateAccessToken(String validApiKey) {
-    Date now =  new Date();
-    Date expired = new Date(now.getTime() + 1000 *  60 * 60);
+        Date now = new Date();
+        Date expired = new Date(now.getTime() + expirationSeconds * 1000);
 
-    return "Bearer " + Jwts.builder()
-            .setSubject("api-auth")
-            .claim("api-auth",  validApiKey)
-            .setIssuedAt(now)
-            .setExpiration(expired)
-            .signWith(secretKey)
-            .compact();
+        return "Bearer " + Jwts.builder()
+                .setSubject("api-auth")
+                .claim("api-auth", validApiKey)
+                .setIssuedAt(now)
+                .setExpiration(expired)
+                .signWith(secretKey)
+                .compact();
     }
 }
