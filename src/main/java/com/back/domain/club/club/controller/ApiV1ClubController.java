7d60672b64afd6e7bdc0f055b64697fa94a059b4
Hyeok Jin Kim <71928299+EpicFn@users.noreply.github.com>
2025-07-29 14:16:44 +0900

    [feat] 모임 소개 - 누구나 (#84)
    * feat : 테스트 케이스 작성
    
    * feat : controller, service 로직 작성
    
    * feat : 테스트 케이스 통과
    
    ---------
    
    Co-authored-by: EpicFn <rlagurwls056@gmail.com>

--- src/main/java/com/back/domain/club/club/controller/ApiV1ClubController.java
+++ src/main/java/com/back/domain/club/club/controller/ApiV1ClubController.java
@@ -60,4 +60,11 @@ public class ApiV1ClubController {
         clubService.deleteClub(clubId);
         return new RsData<>(204, "클럽이 삭제됐습니다.", null);
     }
+
+    @GetMapping("/{clubId}/intro")
+    @Operation(summary = "클럽 소개 조회")
+    public RsData<ClubControllerDtos.ClubIntroResponse> getClubIntro(@PathVariable Long clubId) {
+        ClubControllerDtos.ClubIntroResponse intro = clubService.getClubIntro(clubId);
+        return new RsData<>(200, "클럽 소개 정보가 조회됐습니다.", intro);
+    }
 }
