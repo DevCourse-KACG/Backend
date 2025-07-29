5b23bcd277d3890a67a109271ca7dc6fe2cb885a
sso0om <ssooom142@gmail.com>
2025-07-29 12:08:04 +0900

    feat: 모임으로 모임 참여자 목록 조회 쿼리

--- src/main/java/com/back/domain/club/clubMember/repository/ClubMemberRepository.java
+++ src/main/java/com/back/domain/club/clubMember/repository/ClubMemberRepository.java
@@ -3,6 +3,8 @@ package com.back.domain.club.clubMember.repository;
 import com.back.domain.club.clubMember.entity.ClubMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+import java.util.List;
 
+public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+    List<ClubMember> findByClubId(Long clubId);
 }
