e2448ea0cbe31a84953b893ffef432699022f617
sso0om <ssooom142@gmail.com>
2025-07-30 10:08:58 +0900

    refactor: 코드래빗 적용

--- src/main/java/com/back/domain/member/friend/dto/FriendDto.java
+++ src/main/java/com/back/domain/member/friend/dto/FriendDto.java
@@ -4,22 +4,29 @@ import com.back.domain.member.friend.entity.Friend;
 import com.back.domain.member.friend.entity.FriendStatus;
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.entity.MemberInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record FriendDto(
-    Long friendShipId,
-    Long friendId,
-    String friendNickname,
-    String friendBio,
-    String friendProfileImageUrl,
-    FriendStatus status
+        @Schema(description = "친구 관계 ID")
+        Long friendShipId,
+        @Schema(description = "친구 ID")
+        Long friendId,
+        @Schema(description = "친구 닉네임")
+        String friendNickname,
+        @Schema(description = "친구 자기소개")
+        String friendBio,
+        @Schema(description = "친구 프로필 이미지 URL")
+        String friendProfileImageUrl,
+        @Schema(description = "친구 관계 상태")
+        FriendStatus status
 ) {
     public FriendDto(Friend friend, Member responder, MemberInfo responderInfo) {
         this(
             friend.getId(),
             responder.getId(),
             responder.getNickname(),
-            responderInfo.getBio(),
-            responderInfo.getProfileImageUrl(),
+            responderInfo != null ? responderInfo.getBio() : null,
+            responderInfo != null ? responderInfo.getProfileImageUrl() : null,
             friend.getStatus()
         );
     }
