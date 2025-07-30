dd28960d4efd2c313da688dd5a945476bcb6abee
sso0om <ssooom142@gmail.com>
2025-07-30 10:40:21 +0900

    refactor: 친구 추가 예외 처리 공통 함수

--- src/test/java/com/back/domain/member/friend/controller/ApiV1FriendControllerTest.java
+++ src/test/java/com/back/domain/member/friend/controller/ApiV1FriendControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,6 +30,24 @@ class ApiV1FriendControllerTest {
     private MemberRepository memberRepository;
     @Autowired
     private FriendRepository friendRepository;
+
+    void performErrAddFriend(String friendEmail,
+                          int expectedStatus,
+                          String expectedMessage) throws Exception {
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/members/me/friends")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {"friend_email": "%s"}
+                                """.formatted(friendEmail)))
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1FriendController.class))
+                .andExpect(handler().methodName("addFriend"))
+                .andExpect(status().is(expectedStatus))
+                .andExpect(jsonPath("$.code").value(expectedStatus))
+                .andExpect(jsonPath("$.message").value(expectedMessage));
+    }
 
     @Test
     @DisplayName("친구 추가")
@@ -66,139 +85,65 @@ class ApiV1FriendControllerTest {
     @DisplayName("친구 추가 - 친구 대상이 없는 경우 예외 처리")
     @WithUserDetails(value = "hgd222@test.com")
     void tc2() throws Exception {
-        ResultActions resultActions = mockMvc
-                .perform(post("/api/v1/members/me/friends")
-                        .contentType("application/json")
-                        .content("""
-                                {
-                                    "friend_email": "a@test.com"
-                                }
-                                """)
-                )
-                .andDo(print());
-
-        resultActions
-                .andExpect(handler().handlerType(ApiV1FriendController.class))
-                .andExpect(handler().methodName("addFriend"))
-                .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.code").value(404))
-                .andExpect(jsonPath("$.message").value("친구 대상이 존재하지 않습니다."));
+        performErrAddFriend(
+                "a@test.com",
+                404,
+                "친구 대상이 존재하지 않습니다."
+        );
     }
 
     @Test
     @DisplayName("친구 추가 - 자기 자신을 친구로 추가하는 경우 예외 처리")
     @WithUserDetails(value = "hgd222@test.com")
     void tc3() throws Exception {
-        ResultActions resultActions = mockMvc
-                .perform(post("/api/v1/members/me/friends")
-                        .contentType("application/json")
-                        .content("""
-                                {
-                                    "friend_email": "hgd222@test.com"
-                                }
-                                """)
-                )
-                .andDo(print());
-
-        resultActions
-                .andExpect(handler().handlerType(ApiV1FriendController.class))
-                .andExpect(handler().methodName("addFriend"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.code").value(400))
-                .andExpect(jsonPath("$.message").value("자기 자신을 친구로 추가할 수 없습니다."));
+        performErrAddFriend(
+                "hgd222@test.com",
+                400,
+                "자기 자신을 친구로 추가할 수 없습니다."
+        );
     }
 
     @Test
     @DisplayName("친구 추가 - 이미 친구 요청을 보낸 경우 예외 처리")
     @WithUserDetails(value = "hgd222@test.com")
     void tc4() throws Exception {
-        ResultActions resultActions = mockMvc
-                .perform(post("/api/v1/members/me/friends")
-                        .contentType("application/json")
-                        .content("""
-                                {
-                                    "friend_email": "lyh3@test.com"
-                                }
-                                """)
-                )
-                .andDo(print());
-
-        resultActions
-                .andExpect(handler().handlerType(ApiV1FriendController.class))
-                .andExpect(handler().methodName("addFriend"))
-                .andExpect(status().isConflict())
-                .andExpect(jsonPath("$.code").value(409))
-                .andExpect(jsonPath("$.message").value("이미 친구 요청을 보냈습니다. 상대방의 수락을 기다려주세요."));
+        performErrAddFriend(
+                "lyh3@test.com",
+                409,
+                "이미 친구 요청을 보냈습니다. 상대방의 수락을 기다려주세요."
+        );
     }
 
     @Test
     @DisplayName("친구 추가 - 이미 친구 요청을 받은 경우 예외 처리")
     @WithUserDetails(value = "lyh3@test.com")
     void tc5() throws Exception {
-        ResultActions resultActions = mockMvc
-                .perform(post("/api/v1/members/me/friends")
-                        .contentType("application/json")
-                        .content("""
-                                {
-                                    "friend_email": "hgd222@test.com"
-                                }
-                                """)
-                )
-                .andDo(print());
-
-        resultActions
-                .andExpect(handler().handlerType(ApiV1FriendController.class))
-                .andExpect(handler().methodName("addFriend"))
-                .andExpect(status().isConflict())
-                .andExpect(jsonPath("$.code").value(409))
-                .andExpect(jsonPath("$.message").value("이미 친구 요청을 받았습니다. 수락 또는 거절해주세요."));
+        performErrAddFriend(
+                "hgd222@test.com",
+                409,
+                "이미 친구 요청을 받았습니다. 수락 또는 거절해주세요."
+        );
     }
-
-
 
     @Test
     @DisplayName("친구 추가 - 이미 친구인 경우 예외 처리")
     @WithUserDetails(value = "hgd222@test.com")
     void tc6() throws Exception {
-        ResultActions resultActions = mockMvc
-                .perform(post("/api/v1/members/me/friends")
-                        .contentType("application/json")
-                        .content("""
-                                {
-                                    "friend_email": "cjw5@test.com"
-                                }
-                                """)
-                )
-                .andDo(print());
-
-        resultActions
-                .andExpect(handler().handlerType(ApiV1FriendController.class))
-                .andExpect(handler().methodName("addFriend"))
-                .andExpect(status().isConflict())
-                .andExpect(jsonPath("$.code").value(409))
-                .andExpect(jsonPath("$.message").value("이미 친구입니다."));
+        performErrAddFriend(
+                "cjw5@test.com",
+                409,
+                "이미 친구입니다."
+        );
     }
 
     @Test
     @DisplayName("친구 추가 - 이전에 거절 당한 경우 예외 처리")
     @WithUserDetails(value = "hgd222@test.com")
     void tc7() throws Exception {
-        ResultActions resultActions = mockMvc
-                .perform(post("/api/v1/members/me/friends")
-                        .contentType("application/json")
-                        .content("""
-                                {
-                                    "friend_email": "pms4@test.com"
-                                }
-                                """)
-                )
-                .andDo(print());
-
-        resultActions
-                .andExpect(handler().handlerType(ApiV1FriendController.class))
-                .andExpect(handler().methodName("addFriend"))
-                .andExpect(status().isConflict())
-                .andExpect(jsonPath("$.code").value(409))
-                .andExpect(jsonPath("$.message").value("이전에 거절한 친구 요청입니다. 다시 요청할 수 없습니다."));
+        performErrAddFriend(
+                "pms4@test.com",
+                409,
+                "이전에 거절한 친구 요청입니다. 다시 요청할 수 없습니다."
+        );
     }
 }
