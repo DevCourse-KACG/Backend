bfd7df5d9a3a0384ba6f7cf0725d72ba86e75b00
CYW0815 <cho0815y@gmail.com>
2025-08-05 17:13:32 +0900

    fix 체크리스트 엔티티 수정

--- src/main/java/com/back/domain/checkList/itemAssign/dto/ItemAssignDto.java
+++ src/main/java/com/back/domain/checkList/itemAssign/dto/ItemAssignDto.java
@@ -5,12 +5,14 @@ import com.back.domain.club.clubMember.entity.ClubMember;
 
 public record ItemAssignDto(
     Long id,
+    Long clubMemberId,
     String clubMemberName,
     boolean isChecked
 ) {
   public ItemAssignDto(ItemAssign itemAssign) {
     this(
         itemAssign.getId(),
+        itemAssign.getClubMember().getId(),
         itemAssign.getClubMember().getMember().getNickname(),
         itemAssign.isChecked()
     );
